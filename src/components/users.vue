57c24b826a85d8c5486d04aeb0861cadc39dcb57
zhangxi01 <zhangxi01@qiyi.com>
2018-06-14 18:06:30 +0800

    后台接口联调

--- src/components/users.vue
+++ src/components/users.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="container">
-  <div class="head">选择角色</div>
-  <ul>
-      <li v-for="user in users" :key="user.id" class="line" @click="toInfo(user)">
-        <span v-if="user.sex == 'female'" class="icon-nvtouxiang"></span>
-        <span v-else-if="user.sex == 'male'" class="icon-nantouxiang"></span>
-        <span class="user-content">{{user.userName}}</span>
-      </li>
+    <div class="head">选择角色</div>
+    <ul>
+      <slide-del v-for="user in users" :key="user.id" @click="toInfo(user)" del-text="删除" @del-click="del(user.id)">
+        <div slot="item" class="line">
+          <span v-if="user.sex == 'female'" class="icon-nvtouxiang"></span>
+          <span v-else-if="user.sex == 'male'" class="icon-nantouxiang"></span>
+          <span class="user-content">{{user.userName}}</span>
+        </div>
+      </slide-del>
       <div @click="toInfo" class="button">
         <span class="icon-icon02 add-logo"></span>
         <span class="add-text">增加新用户</span>
@@ -16,8 +18,11 @@
 </template>
 
 <script>
+import SlideDel from '@/components/slideDetele'
+let delFlag = true
 export default {
   name: 'users',
+  components: {SlideDel},
   data () {
     return {
       users: []
@@ -32,6 +37,20 @@ export default {
     }
   },
   methods: {
+    del (id) {
+      if (delFlag) {
+        if (confirm('是否确认删除该用户')) {
+          delFlag = false
+          const delUrl = `${this.$API}/delUser`
+          const _that = this
+          this.$http.post(delUrl, {id: id}).then(function (res) {
+            delFlag = true
+            alert('删除成功')
+            _that.getUser()
+          })
+        }
+      }
+    },
     getUser () {
       const _that = this
       const url = `${this.$API}/getUsers`
